980fd65d741348a2ec610d3aa6993dfdc600b07d
zhangyu <di.li@wehotelglobal.com>
2020-04-22 22:26:33 +0800

    feat: 首页模块路由

--- hkzf-mobile/src/App.js
+++ hkzf-mobile/src/App.js
@@ -1,34 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+
 // 导入首页和城市选择两个组件（页面）
 import Home from './pages/Home'
 import CityList from './pages/CityList'
-
-
-import { Button } from 'antd-mobile'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* 项目根组件 <Button>登录</Button> */}
-
-        {/* 配置导航菜单 */}
-        <div>
-          <div>
-            <Link to="/home">首页</Link>
-          </div>
-          <div>
-            <Link to="/citylist">城市选择</Link>
-          </div>
-        </div>
-        
+        {/* 配置路由 */}
+        <Route path="/" exact render={() =>
+          <Redirect to="/home" />
+        }></Route>
+        {/* Home 组件是父路由的内容 */}
         <Route path="/home" component={Home} />
         <Route path="/citylist" component={CityList} />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
