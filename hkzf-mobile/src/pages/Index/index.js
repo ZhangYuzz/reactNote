c51bc947a3b2bfad008f26d964fa8386880bed66
zhangyu <di.li@wehotelglobal.com>
2020-05-20 10:13:32 +0800

    feat: 表单处理

--- hkzf-mobile/src/pages/Index/index.js
+++ hkzf-mobile/src/pages/Index/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 // 导入组件
-import { Carousel, Flex } from 'antd-mobile'
+import { Carousel, Flex, Grid, WingBlank } from 'antd-mobile'
 
 // 导入axios
 import axios from 'axios'
+
+// 导入
+import { BASE_URL } from '../../utils/url'
 
 // 导入导航菜单图片
 import Nav1 from '../../assets/images/nav-1.png'
@@ -14,6 +17,12 @@ import Nav4 from '../../assets/images/nav-4.png'
 
 // 导入样式文件
 import './index.scss'
+
+// 导入utils中获取定位城市的方法
+import { getCurrentCity } from '../../utils'
+
+// 导入搜索导航栏组件
+import SearchHeader from '../../components/SearchHeader'
 
 // 导航菜单数据
 const navs = [
@@ -43,11 +52,30 @@ const navs = [
   }
 ]
 
+// 获取地理位置信息
+// navigator.geolocation.getCurrentPosition(position => {
+//   console.log('当前位置信息：', position)
+// })
+
+/* 
+  1 打开百度地图JS API 定位文档 。
+  2 通过 IP 定位获取到当前城市名称。
+  3 调用我们服务器的接口，换取项目中的城市信息（有房源的城市的名称和id）。
+  4 将接口返回的城市信息展示在顶部导航栏中。
+*/
+
 export default class Index extends React.Component {
   state = {
     // 轮播图状态数据
     swipers: [],
-    isSwiperLoaded: false
+    isSwiperLoaded: false,
+
+    // 租房小组数据
+    groups: [],
+    // 最新资讯
+    news: [],
+    // 当前城市名称
+    curCityName: '上海'
   }
 
   // 获取轮播图数据的方法
@@ -59,8 +87,53 @@ export default class Index extends React.Component {
     })
   }
 
-  componentDidMount() {
+  // 获取租房小组数据的方法
+  async getGroups() {
+    const res = await axios.get('http://localhost:8080/home/groups', {
+      params: {
+        area: 'AREA%7C88cff55c-aaa4-e2e0'
+      }
+    })
+
+    // console.log(res)
+    this.setState({
+      groups: res.data.body
+    })
+  }
+
+  // 获取最新资讯
+  async getNews() {
+    const res = await axios.get(
+      'http://localhost:8080/home/news?area=AREA%7C88cff55c-aaa4-e2e0'
+    )
+
+    this.setState({
+      news: res.data.body
+    })
+  }
+
+  async componentDidMount() {
     this.getSwipers()
+    this.getGroups()
+    this.getNews()
+
+    // 2 通过 IP 定位获取到当前城市名称。
+    // const curCity = new window.BMap.LocalCity()
+    // curCity.get(async res => {
+    //   // console.log('当前城市信息：', res)
+    //   const result = await axios.get(
+    //     `http://localhost:8080/area/info?name=${res.name}`
+    //   )
+    //   // console.log(result)
+    //   this.setState({
+    //     curCityName: result.data.body.label
+    //   })
+    // })
+
+    const curCity = await getCurrentCity()
+    this.setState({
+      curCityName: curCity.label
+    })
   }
 
   // 渲染轮播图结构
@@ -76,7 +149,7 @@ export default class Index extends React.Component {
         }}
       >
         <img
-          src={`http://localhost:8080${item.imgSrc}`}
+          src={BASE_URL + item.imgSrc}
           alt=""
           style={{ width: '100%', verticalAlign: 'top' }}
         />
@@ -97,6 +170,28 @@ export default class Index extends React.Component {
     ))
   }
 
+  // 渲染最新资讯
+  renderNews() {
+    return this.state.news.map(item => (
+      <div className="news-item" key={item.id}>
+        <div className="imgwrap">
+          <img
+            className="img"
+            src={`http://localhost:8080${item.imgSrc}`}
+            alt=""
+          />
+        </div>
+        <Flex className="content" direction="column" justify="between">
+          <h3 className="title">{item.title}</h3>
+          <Flex className="info" justify="between">
+            <span>{item.from}</span>
+            <span>{item.date}</span>
+          </Flex>
+        </Flex>
+      </div>
+    ))
+  }
+
   render() {
     return (
       <div className="index">
@@ -109,10 +204,43 @@ export default class Index extends React.Component {
           ) : (
             ''
           )}
+
+          {/* 搜索框 */}
+          <SearchHeader cityName={this.state.curCityName} />
         </div>
 
         {/* 导航菜单 */}
         <Flex className="nav">{this.renderNavs()}</Flex>
+
+        {/* 租房小组 */}
+        <div className="group">
+          <h3 className="group-title">
+            租房小组 <span className="more">更多</span>
+          </h3>
+
+          {/* 宫格组件 */}
+          <Grid
+            data={this.state.groups}
+            columnNum={2}
+            square={false}
+            hasLine={false}
+            renderItem={item => (
+              <Flex className="group-item" justify="around" key={item.id}>
+                <div className="desc">
+                  <p className="title">{item.title}</p>
+                  <span className="info">{item.desc}</span>
+                </div>
+                <img src={`http://localhost:8080${item.imgSrc}`} alt="" />
+              </Flex>
+            )}
+          />
+        </div>
+
+        {/* 最新资讯 */}
+        <div className="news">
+          <h3 className="group-title">最新资讯</h3>
+          <WingBlank size="md">{this.renderNews()}</WingBlank>
+        </div>
       </div>
     )
   }
--- hkzf-mobile/src/pages/News/index.js
+++ hkzf-mobile/src/pages/News/index.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react'
 
 export default class News extends React.Component {
-  render () {
+  render() {
     return (
-      <div>sds</div>
+      <div style={{ backgroundColor: 'green' }}>
+        这是 News组件 的内容，它是子路由的内容
+      </div>
     )
   }
 }
